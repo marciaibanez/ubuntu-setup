953d2ecef0c83f6a3b3725f6989b67a93617db66
Marcia Ibanez <marcia.ibanez.1@gmail.com>
2019-09-02 11:35:09 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ apt install -y \
      make \
      zsh
 
-echo "auto-remove"
+echo "Running auto-remove"
 apt autoremove -y
 apt autoclean
 
@@ -52,31 +52,31 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 
-echo "git config"
+echo "Configuring git"
 git config --global user.email "marcia.ibanez.1@gmail.com"
 git config --global user.name "Marcia Ibanez"
 
-echo "Installing Docker"
+echo "Installing docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 rm get-docker.sh
 usermod -aG docker marciaibanez
 
-echo "Installing Node"
+echo "Installing node"
 curl -sL https://deb.nodesource.com/setup_12.x | bash
 apt install nodejs
 
 echo "Installing zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "Installing Docker Compose"
+echo "Installing docker compose"
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-echo "Making Linux use Local Time..."
+echo "Making linux use local time..."
 timedatectl set-local-rtc 1 --adjust-system-clock
 
-echo "Installing Fallout grub theme"
+echo "Installing fallout grub theme"
 wget -O - https://github.com/shvchk/fallout-grub-theme/raw/master/install.sh | bash
 
 echo "Installing Stacer"
