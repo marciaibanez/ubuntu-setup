940e72b632ad7c64c247dd806d257f7fad202c21
Marcia Ibanez <contato@marciaibanez.com>
2021-08-15 17:47:51 -0300

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -129,6 +129,16 @@ install_discord() {
   sudo rm discord.deb
 }
 
+install_vscode() {
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  rm -f packages.microsoft.gpg
+  sudo apt install -y apt-transport-https
+  sudo apt update
+  sudo apt install -y code
+}
+
 setup() {
   echo "\n Marcia's Ubuntu 21.04 Setup"
 
@@ -163,6 +173,7 @@ setup() {
   install_telegram
   install_spotify
   install_discord
+  install_vscode
 
   step "Configure date to use Local Time"
   sudo timedatectl set-local-rtc 1 --adjust-system-clock
