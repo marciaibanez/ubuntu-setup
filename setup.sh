95d77c1dff87924c2d65f5b92cf0f1832c3d638c
Marcia Ibanez <marcia.ibanez.1@gmail.com>
2021-08-26 17:13:28 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -139,6 +139,13 @@ install_vscode() {
   sudo apt install -y code
 }
 
+install_gcloud_sdk() {
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  sudo apt install -y apt-transport-https ca-certificates gnupg
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+  sudo apt-get update && sudo apt-get install google-cloud-sdk
+}
+
 setup() {
   echo "\n Marcia's Ubuntu 21.04 Setup"
 
@@ -174,6 +181,7 @@ setup() {
   install_spotify
   install_discord
   install_vscode
+   install_gcloud_sdk
 
   step "Configure date to use Local Time"
   sudo timedatectl set-local-rtc 1 --adjust-system-clock
